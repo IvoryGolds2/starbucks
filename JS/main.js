01a0578c05e58f4f0055ddcd892d35fad5d990ba
IvoryGolds2 <sakuravely@naver.com>
2023-04-13 18:28:03 +0900

    목요일 쥬교

--- JS/main.js
+++ JS/main.js
@@ -20,3 +20,20 @@ searchinputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchinputEl.setAttribute('placeholder', ''); //HTML 속성을 추가하는 메소드
 })
+
+// 스크롤 시 전역 배지(고정 배너) 숨기기
+const badgeEl = document.querySelector('header .badges');
+
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function (){
+  // console.log(window.screenY); //y축으로 얼마나 스크롤 했는지에 대한 수치
+
+  // 만약 스크롤의 위치가 500을 넘으면 배지 요소를 숨기고
+  // 그렇지 않으면 다시 보이기!!
+  if (window.scrollY > 500 ) {
+    // 배지 요소 숨기기!
+    badgeEl.style.display = 'none';
+  } else {
+    badgeEl.style.display = 'block';
+  }
+});
