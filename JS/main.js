3bf2033fe1d78277bcf8cdfaca957d026eca5af0
IvoryGolds2 <sakuravely@naver.com>
2023-04-14 18:29:53 +0900

    어렵지롱

--- JS/main.js
+++ JS/main.js
@@ -32,8 +32,42 @@ window.addEventListener('scroll', function (){
   // 그렇지 않으면 다시 보이기!!
   if (window.scrollY > 500 ) {
     // 배지 요소 숨기기!
-    badgeEl.style.display = 'none';
+    //badgeEl.style.display = 'none';
+
+    //gsap.to(요소, 지속시간, 옵션: {}) 메소드: CSS 속성을 통해 애니메이션 처리
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
   } else {
-    badgeEl.style.display = 'block';
-  }
+    //배지 요소 보이기!
+    //badgeEl.style.display = 'block';
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display:'block'
+  });
+}
 });
+
+//순차적으로 VISUAL 섹션 내 요소 보이기
+// 나타낼 요소(.fade-in)들을 찾기
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+
+//요소들을 하나씩 반복해서 처리
+fadeEls.forEach(function (fadeE1, index) {
+  // gsap.to(요소, 지속시간, 옵션 :{})
+  gsap.to(fadeE1, 1, {
+    delay: (index + 1) * 0.7, //0.7, 1.4, 2.1, 2.8 
+    opacity: 1
+  });
+});
+
+// 공지사항 수직 슬라이드 기능 작성
+// new 키워드로 Swiper 객체를 생성 => 슬라이드 기능을 생성
+//new Swiper(선택자, 옵션: {});
+new Swiper('.notice .swiper', {
+  direction: 'vertical', //수직 슬라이드
+  loop: true, //반복 재생 여부
+  autoplay: true, //자동 재생 여부
+});//수직 슬라이드는 꼭 높이를 지정하지 않으면 매우 곤란
